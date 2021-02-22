489ecdde22a4c8a556974c2ad5997918b3dee67c
emchudd <eclhuddleston@gmail.com>
2021-02-22 13:21:55 -0600

    Completed JavaScript Exercise

---
+++ jsBasicsExercise.js
@@ -0,0 +1,39 @@
+console.log("13");
+
+// Five Data Types
+//Numbers
+//Strings
+//Booleans
+//Null
+//Undefined
+
+// Six Operator Symbols
+// + is addition
+// - is subtraction
+// * is multiplication
+// / is division
+// ** is exponentation
+// % is modulus (remainder)
+
+console.log(6%5);
+console.log(5%2);
+
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+
+console.log("Wanda" + "Vision");
+
+console.log("Emily".length + "Huddleston".length);
+
+console.log("Avengers:Endgame"[15]);
+
+//BONUS
+
+console.log("\\");
+
+console.log(5 ** 4 - 9)
+
+// 3 console.log();
+
+// 4 
+
+// 5 
